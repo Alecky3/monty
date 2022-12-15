aea23f170350dfe56a8b85c466de8dd956068780
alex <alexmuthini3@gmail.com>
2022-12-15 15:25:25 +0300

    corrected betty-sttyle warnings

--- monty.c
+++ monty.c
@@ -75,7 +75,7 @@ void f_mod(stack_t **head, unsigned int counter)
  * f_mul - multiplies the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
- * Return: no return
+ * Return: void
 */
 void f_mul(stack_t **head, unsigned int counter)
 {
